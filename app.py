cf7bfa1ed3f1bc50681af8fda09e09651479ab6f
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-02-14 10:45:44 -0500

    generated graph

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import scrapy
 import json
 import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 from time import sleep
 
 class BusinessSearchSpider(scrapy.Spider):
@@ -28,20 +31,42 @@ class BusinessSearchSpider(scrapy.Spider):
                 endpoint, callback=self.process_company_data, 
                 headers=self.headers, meta={'company': data[business_id]['TITLE'][0]}
             )
+            break
 
     def process_company_data(self, response):
         sleep(.1)
         new_row = {
-            'Company': response.meta['company'], 'Commercial Registered Agent': None, 
-            'Registered Agent': None, 'Owner': None
+            'Company': response.meta['company'].upper(), 'Commercial Registered Agent': None, 
+            'Registered Agent': None, 'Owners': None
         }
         for r in response.json()['DRAWER_DETAIL_LIST']:
-            if r['LABEL'] == 'Commercial Registered Agent': new_row['Commercial Registered Agent'] = r['VALUE'].replace('\n', ' ')
-            if r['LABEL'] == 'Registered Agent,': new_row['Registered Agent'] = r['VALUE']
-            if r['LABEL'] == 'Owner Name': new_row['Owner'] = r['VALUE']
+            if r['LABEL'] == 'Commercial Registered Agent': 
+                new_row['Commercial Registered Agent'] = r['VALUE'][:r['VALUE'].index('\n')].upper()
+            if r['LABEL'] == 'Registered Agent,': 
+                new_row['Registered Agent'] = r['VALUE'].upper()
+            if r['LABEL'] == 'Owner Name': 
+                new_row['Owners'] = r['VALUE'].upper()
         self.company_rows.append(new_row)
      
     def closed(self, reason):
-        df = pd.DataFrame(self.company_rows)
-        df.to_csv('data.csv', index=False)
-        print(df.head(20))
+        # df = pd.DataFrame(self.company_rows)
+        # df.to_csv('data.csv', index=False)
+        self.generate_graph(None)
+    
+    def generate_graph(self, df):
+        df = pd.read_csv('data.csv')
+        commercial_registered_agent_df = df[df['Commercial Registered Agent'].notnull()]
+        registered_agent_df = df[df['Registered Agent'].notnull()]
+        owners_df = df[df['Owners'].notnull()]
+        g = nx.Graph()
+        g.add_nodes_from(df['Company'])
+        g2 = nx.from_pandas_edgelist(commercial_registered_agent_df, source='Company', target='Commercial Registered Agent')
+        g3 = nx.from_pandas_edgelist(registered_agent_df, source='Company', target='Registered Agent')
+        g4 = nx.from_pandas_edgelist(owners_df, source='Company', target='Owners')
+        combined = nx.compose(g, g2)
+        combined2 = nx.compose(combined, g3)
+        all_nodes = nx.compose(combined2, g4)
+        pos = nx.spring_layout(all_nodes, k=.2)
+        plt.figure(figsize=(7,7)) 
+        nx.draw(all_nodes, pos, node_size=35, font_size=2.25, with_labels=True)
+        plt.savefig('graph.png', dpi=500)
